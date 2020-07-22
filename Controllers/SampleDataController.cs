55ed7c0283b4f487b13600e3afaf32312f36dc34
DanitLugasi <danitgolan@gmail.com>
2020-07-23 00:07:32 +0300

    finish

---
+++ Controllers/SampleDataController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MLY.Model;
+
+namespace MLY.Controllers
+{
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetData()
+        {
+            try
+            {
+                string url = "https://jsonplaceholder.typicode.com/users";
+                HttpClient client = new HttpClient();
+                HttpResponseMessage message = client.GetAsync(url).Result;
+                message.EnsureSuccessStatusCode();
+                var result = message.Content.ReadAsStringAsync().Result;
+                List<Data> data = JsonSerializer.Deserialize<List<Data>>(result);
+                return Ok(data);
+            }
+            catch
+            {
+                return BadRequest("Internal server error");
+            }
+        }
+
+    }
+}
---
+++ Model/Data.cs
@@ -0,0 +1,28 @@
+﻿
+namespace MLY.Model
+{
+    public class Data
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public Address address { get; set; }
+        public Company company { get; set; }
+    }
+
+    public class Company
+    {
+        public string name { get; set; }
+    }
+
+    public class Address
+    {
+        public string suite { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+    }
+
+}
